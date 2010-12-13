e8f7f6dccf24b117c7982544e21dd6564afdaedd
Alex Drummond <a.d.drummond@gmail.com>
2010-12-13 18:08:55 -0500

    Add disasm.c

---
+++ disasm.c
@@ -0,0 +1,25 @@
+//
+// See http://udis86.sourceforge.net/manual/manual.html
+// Useful little dissassembling utility.
+//
+// gcc -Iudis86_includes_dir -Ludis86_libs_dir -ludis86 disasm.c -o disasm
+//
+
+#include <stdio.h>
+#include <udis86.h>
+
+int main()
+{
+    ud_t ud_obj;
+
+    ud_init(&ud_obj);
+    ud_set_input_file(&ud_obj, stdin);
+    ud_set_mode(&ud_obj, 64);
+    ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+    while (ud_disassemble(&ud_obj)) {
+        printf("\t%s\n", ud_insn_asm(&ud_obj));
+    }
+
+    return 0;
+}
